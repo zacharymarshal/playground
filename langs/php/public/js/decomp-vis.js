b683f41f0ef36eb9d0db2ccaebee671d25090776
Zachary Rankin <rankin.zachary@gmail.com>
2024-11-15 23:18:41 -0800

    Added some animation to things

--- langs/php/public/js/decomp-vis.js
+++ langs/php/public/js/decomp-vis.js
@@ -1,3 +1,44 @@
+class Wait {
+  completed = false;
+  constructor(startTime, duration, onDone) {
+    this.duration = duration;
+    this.onDone = onDone;
+    this.startTime = startTime;
+  }
+
+  update(ts) {
+    if (this.completed) {
+      return true;
+    }
+
+    if (ts - this.startTime >= this.duration) {
+      this.completed = true;
+      this.onDone();
+      return true;
+    }
+
+    return false;
+  }
+}
+
+const animate = (...anims) => {
+  let currentAnimationIdx = 0;
+
+  const next = () => {
+    console.log("next called");
+    currentAnimationIdx++;
+  };
+
+  return () => {
+    console.log("currentAnimationIdx", currentAnimationIdx);
+    anims.forEach((anim, idx) => {
+      if (idx <= currentAnimationIdx) {
+        anim(next);
+      }
+    });
+  };
+};
+
 const drawLine = (p, x1, y1, x2, y2) => {
   p.stroke(0);
   p.strokeWeight(2);
@@ -45,14 +86,23 @@ const drawCircle = (p, x, y, radius, fill = false) => {
   p.endShape(p.CLOSE);
 };
 
-const drawMarker = (p, x, y, text) => {
-  drawCircle(p, x, y, 5, true);
-  p.noStroke();
-  p.textFont("Comic Sans MS");
+const drawMarkerLine = (p, x, y, length) => {
+  p.stroke(0);
+  p.strokeWeight(2);
+  p.line(x - length / 2, y, x + length / 2, y);
+};
+
+const drawMarkerCircle = (p, x, y, radius) => {
+  drawCircle(p, x, y, radius, true);
+};
+
+const drawMarkerLabel = (p, x, y, text) => {
+  p.textFont("Schoolbell");
   p.textSize(21);
   p.fill(0);
-  p.textAlign(p.CENTER, p.CENTER);
-  p.text(text, x + 25, y + 3);
+  p.noStroke();
+  p.textAlign(p.LEFT, p.CENTER);
+  p.text(text, x + 16, y + 3);
 };
 
 const drawArrow = (p, position, target) => {
@@ -96,10 +146,61 @@ const drawOpenNumberLine = (p, points) => {
   );
 };
 
-const drawArc = (p, x, y1, y2, label = "") => {
+const drawArc = (p, jumpProgress, x, y1, y2, label = "") => {
   const randomness = 1;
   const segments = 30;
   const arcWidth = 60;
+
+  p.noFill();
+  p.stroke(0);
+  p.strokeWeight(2);
+  p.beginShape();
+
+  for (let i = 0; i < Math.min(31, segments * jumpProgress); i++) {
+    t = i / segments;
+
+    const ny = p.lerp(y1, y2, t);
+
+    const offset = Math.sin(t * Math.PI) * arcWidth;
+    const nx = x - offset + p.random(-randomness, randomness);
+    const wobblyY = ny + p.random(-randomness, randomness);
+
+    p.vertex(nx, wobblyY);
+  }
+
+  p.endShape();
+
+  if (label) {
+    p.textFont("Schoolbell");
+    p.textSize(21);
+    p.fill(0);
+    p.noStroke();
+    p.textAlign(p.RIGHT, p.CENTER);
+    p.text(label, x - arcWidth - 24, (y1 + y2) / 2);
+  }
+};
+
+const drawJump = (p, jumpProgress, x, y1, y2, label = "", onDone = null) => {
+  drawArc(p, jumpProgress, x - 12, y1, y2 - 8, label);
+  if (jumpProgress >= 1) {
+    drawArrow(p, { x: x - 18, y: y2 - 14 }, { x: x - 8, y: y2 - 4 });
+  }
+};
+
+const drawAnimatedJump = (
+  p,
+  progress,
+  x,
+  y1,
+  y2,
+  label = "",
+  onDone = () => {},
+) => {
+  const arcWidth = 60;
+  const randomness = 1;
+  const segments = 30;
+
+  const ny = p.lerp(y1, y2, progress);
 
   p.noFill();
   p.stroke(0);
@@ -121,18 +222,17 @@ const drawArc = (p, x, y1, y2, label = "") => {
   p.endShape();
 
   if (label) {
-    p.textFont("Comic Sans MS");
+    p.textFont("Schoolbell");
     p.textSize(21);
     p.fill(0);
     p.noStroke();
     p.textAlign(p.RIGHT, p.CENTER);
     p.text(label, x - arcWidth - 24, (y1 + y2) / 2);
   }
-};
 
-const drawJump = (p, x, y1, y2, label = "") => {
-  drawArc(p, x - 12, y1, y2 - 8, label);
-  drawArrow(p, { x: x - 18, y: y2 - 14 }, { x: x - 8, y: y2 - 4 });
+  if (progress >= 1) {
+    onDone();
+  }
 };
 
 const decomposeViz = (p) => {
@@ -140,23 +240,39 @@ const decomposeViz = (p) => {
   const height = 800;
   const padding = 50;
 
-  const subtrahend = 18;
-  const minuend = 32;
-  const difference = minuend - subtrahend;
+  const problems = [
+    {
+      minuend: 32,
+      subtrahend: 18,
+      difference: 14,
+      markers: [{ value: 32 }, { value: 22 }, { value: 20 }, { value: 14 }],
+      jumps: [
+        { from: 0, to: 1, label: "- 10" },
+        { from: 1, to: 2, label: "- 2" },
+        { from: 2, to: 3, label: "- 6" },
+      ],
+    },
+    {
+      minuend: 43,
+      subtrahend: 27,
+      difference: 16,
+      markers: [{ value: 43 }, { value: 23 }, { value: 20 }, { value: 16 }],
+      jumps: [
+        { from: 0, to: 1, label: "- 20" },
+        { from: 1, to: 2, label: "- 3" },
+        { from: 2, to: 3, label: "- 4" },
+      ],
+    },
+  ];
 
-  const numberLine = {
-    markers: [
-      { value: minuend },
-      { value: minuend - 10 },
-      { value: minuend - 10 - 2 },
-      { value: difference },
-    ],
-    jumps: [
-      { from: 0, to: 1, label: "- 10" },
-      { from: 1, to: 2, label: "- 2" },
-      { from: 2, to: 3, label: "- 6" },
-    ],
-  };
+  const problemIdx = window.location.hash.slice(1);
+  const problem = problems[problemIdx];
+
+  const subtrahend = problem.subtrahend;
+  const minuend = problem.minuend;
+  const difference = problem.difference;
+
+  const numberLine = problem;
   numberLine.markers.forEach((marker, i) => {
     const y = p.map(
       marker.value,
@@ -171,15 +287,68 @@ const decomposeViz = (p) => {
 
   const lineLength = height - padding * 2;
   const fps = 12;
+  const anims = [];
+  let a;
 
   p.setup = () => {
     p.createCanvas(width, height);
     p.smooth();
     p.frameRate(fps);
+
+    numberLine.markers.forEach((marker, idx) => {
+      let done1;
+      anims.push(function a(next) {
+        drawMarkerLine(p, marker.x, marker.y, 20);
+        if (!done1) {
+          done1 = true;
+          setTimeout(next, 1000);
+        }
+      });
+      let done;
+      anims.push(function b(next) {
+        if (idx === 0 || idx === numberLine.markers.length - 1) {
+          drawMarkerCircle(p, marker.x, marker.y, 6);
+        }
+        drawMarkerLabel(p, marker.x, marker.y, marker.value);
+        if (!done) {
+          done = true;
+          setTimeout(next, 1000);
+        }
+      });
+      let done2;
+      let jumpProgress = 0;
+      anims.push(function b(next) {
+        const jump = numberLine.jumps.find((jump) => jump.from === idx);
+        if (!jump) {
+          return;
+        }
+        const to = numberLine.markers[jump.to];
+        drawJump(p, jumpProgress, width / 2 - 4, marker.y, to.y, jump.label);
+        if (jumpProgress < 1) {
+          jumpProgress += 0.1;
+        }
+        drawMarkerLine(p, to.x, to.y, 20);
+        if (!done2) {
+          done2 = true;
+          setTimeout(next, 1000);
+        }
+      });
+    });
+    console.log(anims);
+    a = animate(...anims);
   };
 
   p.draw = () => {
     p.clear();
+
+    // draw the problem minuend - subtrahend = difference
+    p.textFont("Schoolbell");
+    p.textSize(32);
+    p.fill(0);
+    p.noStroke();
+    p.textAlign(p.CENTER, p.CENTER);
+    p.text(`${minuend} - ${subtrahend} =`, width / 2, 20);
+
     drawOpenNumberLine(p, {
       x1: width / 2,
       y1: padding,
@@ -187,15 +356,21 @@ const decomposeViz = (p) => {
       y2: height - padding,
     });
 
-    numberLine.markers.forEach((marker) => {
-      drawMarker(p, marker.x, marker.y, marker.value);
-    });
+    a();
 
-    numberLine.jumps.forEach((jump) => {
-      const from = numberLine.markers[jump.from];
-      const to = numberLine.markers[jump.to];
-      drawJump(p, width / 2, from.y, to.y, jump.label);
-    });
+    // numberLine.markers.forEach((marker, idx) => {
+    //   drawMarkerLine(p, marker.x, marker.y, 20);
+    //   if (idx === 0 || idx === numberLine.markers.length - 1) {
+    //     drawMarkerCircle(p, marker.x, marker.y, 6);
+    //   }
+    //   drawMarkerLabel(p, marker.x, marker.y, marker.value);
+    // });
+
+    // numberLine.jumps.forEach((jump) => {
+    //   const from = numberLine.markers[jump.from];
+    //   const to = numberLine.markers[jump.to];
+    //   drawJump(p, width / 2 - 4, from.y, to.y, jump.label);
+    // });
   };
 };
 
